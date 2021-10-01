ecd842a36ded000159d0f4a018aec1087fd632cf
dylee111 <76557211+dylee111@users.noreply.github.com>
2021-10-01 15:20:58 +0900

    2021/10/01

--- src/main/java/org/zerock/club/entity/ClubMember.java
+++ src/main/java/org/zerock/club/entity/ClubMember.java
@@ -26,7 +26,7 @@ public class ClubMember extends BaseEntity {
 
     private boolean fromSocial;
 
-    @ElementCollection(fetch = FetchType.LAZY) // One To Many 관계를 다룸, 하지만 Entity가 닌 단순 객체 집합을 다룰 때 사용
+    @ElementCollection(fetch = FetchType.LAZY) // One To Many 관계를 다룸, 하지만 Entity가 아닌 단순 객체 집합을 다룰 때 사용
     @Builder.Default
     private Set<ClubMemberRole> roleSet = new HashSet<>(); // 복수의 role을 가질 수 있기 때문에 set 타입으로 정의
 
